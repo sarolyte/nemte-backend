e1c07f6d5cc79334dfca9293c951f771ab3071c3
sarolyte <meda.sarolyte@gmail.com>
2025-10-07 19:53:09 +0300

    validateSchema creation

---
+++ middleware/validateSchema.js
@@ -0,0 +1,13 @@
+export const validateSchema = (schema) => {
+    return (req, res, next) => {
+        const {error, value} = schema.validate(req.body, {abortEarly: false});
+
+        if (error) {
+            const messages = error.details.map(detail => detail.message);
+            return res.status(400).json({error: messages})
+        }
+
+        req.validateSchema = value;
+        next();
+    }
+}
