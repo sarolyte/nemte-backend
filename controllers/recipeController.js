b0c0efb5149ec781474e0c87380370850db2f464
sarolyte <meda.sarolyte@gmail.com>
2025-10-07 22:47:19 +0300

    add getRecipeById controller, GET by id recipe route and corect typo in allRecipes router for word "length"

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import { getDB } from "../db/connect.js";
 
 //POST
@@ -25,12 +26,13 @@ export const createRecipe = async (req, res) => {
 export const allRecipes = async (req, res) => {
     const db = getDB();
     try {
-        const recipes = await db.collection('recipes')
+        const recipes = await db
+            .collection('recipes')
             .find()
             .limit(15)
             .toArray();
 
-        if (recipes.lenght === 0) {
+        if (recipes.length === 0) {
             return res.status(200).json({
                 message: 'No recipe were found', 
                 data: []
@@ -45,5 +47,34 @@ export const allRecipes = async (req, res) => {
     } catch (err) {
         console.error('Error fetching recipes:', err.message)
         res.status(500).json({err: 'Unable to fetch recipes'});
+    }
+}
+
+//GET id
+export const getRecipeById = async (req, res) => {
+
+    try {
+        const recipeId = req.params.id;
+
+        if (!ObjectId.isValid(recipeId)) {
+            return res.status(400).json({ error: 'Invalid ID format' })
+        };
+
+        const db = getDB();
+
+        const recipe = await db.collection('recipes').findOne({_id: new ObjectId(recipeId)});
+
+        if (!recipe) {
+            return res.status(404).json({ message: 'No recips was found with this ID' })
+        }
+
+        res.status(200).json({
+            message: 'A recipe was found',
+            data: recipe
+        })
+
+    } catch (err) {
+        console.error('Error fetching recipe:', err.message)
+        res.status(500).json({err: 'Unable to fetch recipe'});
     }
 }
--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { validateSchema } from '../middleware/validateSchema.js';
 import { recipeSchema } from '../schemas/recipeSchema.js';
-import { allRecipes, createRecipe } from '../controllers/recipeController.js';
+import { allRecipes, createRecipe, getRecipeById } from '../controllers/recipeController.js';
 
 const recipeRouter = Router();
 
 recipeRouter.post('/recipe', validateSchema(recipeSchema), createRecipe);
 recipeRouter.get('/recipes', allRecipes);
+recipeRouter.get('/recipe/:id', getRecipeById);
 
 
 export default recipeRouter;
