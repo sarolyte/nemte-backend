225e140e78cf42bbfd88ec2e633447697d04616d
sarolyte <meda.sarolyte@gmail.com>
2025-10-11 00:18:33 +0300

    add image and description to recipeSchema

--- schemas/recipeSchema.js
+++ schemas/recipeSchema.js
@@ -7,6 +7,8 @@ export const recipeSchema = Joi.object({
   createdAt: Joi.date().optional(),
   updatedAt: Joi.date().optional(),
   name: Joi.string().min(5).max(55).required(),
+  image: Joi.string().uri().required(),
+  description: Joi.string().min(10).max(450).required(),
   cookingDuration: durationSchema.required(),
   cleaningTime: durationSchema.required(),
   ingredients: Joi.array().items(ingredientSchema).min(1).required(),
